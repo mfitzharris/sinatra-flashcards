4e68af888880e5ccd88a1bf1fb9b204efa56ef5c
Matt Grimm <m.m.grimm1@gmail.com>
2016-10-07 15:45:38 -0500

    Create guess migration table & migrate. Create guess model and associations

---
+++ app/models/guess.rb
@@ -0,0 +1,7 @@
+class Guess < ActiveRecord::Base
+  belongs_to :round, :card
+  belongs_to :user, through: :round
+
+  #attributes are: round_id, card_id, correct, counter
+  #correct is type boolean with default value of false
+end
