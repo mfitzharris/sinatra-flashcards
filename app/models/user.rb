4e297e40826aa5ecc5365cfff6657596c45889c4
David Rodgers <dtrodger20@gmail.com>
2016-10-07 15:28:40 -0500

    Add User model.

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+
+	has_many :entries
+
+	validates :username, :email, :password_hash, :presence => true
+
+	validates :username, :email, :uniqueness => true
+
+	def password
+		@password ||= BCrypt::Password.new(self.password_hash)
+	end
+
+	def password=(new_password)
+		password = BCrypt::Password.create(new_password)
+		self.password_hash = password
+	end
+
+	def authenticate(plain_text_password)
+		self.password == plain_text_password
+	end
+
+end
