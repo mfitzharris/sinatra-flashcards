dbd0e97e3d242a6043efb95e402f123c2146b032
David Rodgers <dtrodger20@gmail.com>
2016-10-07 22:38:42 -0500

    Started round controller. Unsure about it. Refactored round model to fix belongs to.

---
+++ app/controllers/round_controller.rb
@@ -0,0 +1,11 @@
+# post 'decks/:deck_id/round' do
+
+# 	#remove this query once we get the current_user helper working
+# 	user = User.find_by(user_id: session[:user_id])
+# 	if user
+# 		round = Round.create(deck_id: params[:deck_id], user_id: user.id)
+# 	else
+# 		round = Round.create(deck_id: params[:deck_id])
+# 	end
+
+# end
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,5 +1,6 @@
 class Round < ActiveRecord::Base
-  belongs_to :deck, :user
+  belongs_to :deck
+  belongs_to :user
   has_many :guesses
 
   #Note, only attributes are deck_id and user_id
