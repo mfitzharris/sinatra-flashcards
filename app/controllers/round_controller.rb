e2fac3f349d85cf33c3e25e644f2d10c3028dbf4
David Rodgers <dtrodger20@gmail.com>
2016-10-07 22:59:13 -0500

    Tested post to /round works as intended.

--- app/controllers/round_controller.rb
+++ app/controllers/round_controller.rb
@@ -1,9 +1,10 @@
 #Instead of posting to a decks controller to create a round as we had discussed, it seems restful to post to a round route to create the round. Im open to ideas on this.
 
-post 'decks/:deck_id/round' do
+post '/decks/:deck_id/round' do
 
 	#remove this query once we get the current_user helper working
-	user = User.find_by(user_id: session[:user_id])
+	user = User.find_by(id: session[:user_id])
+	
 	if user
 		round = Round.create(deck_id: params[:deck_id], user_id: user.id)
 	else
@@ -11,7 +12,7 @@ post 'decks/:deck_id/round' do
 	end
 
 	#MVP feature. Final project should shuffle the deck then return the card id of the first card in the shuffled deck
-	card_id = Deck.find(params[:deck_id]).cards.first
+	card_id = Deck.find(params[:deck_id]).cards.first.id
 
 	redirect "decks/#{params[:deck_id]}/cards/#{card_id}"
 
